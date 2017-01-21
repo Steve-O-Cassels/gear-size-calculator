5d6e7c4e79400b732b64648f13a51bcf15702165
Stephen Cassels <steve.o.cassels@gmail.com>
2017-01-21 09:11:46 +0000

    Improve testability of cassette by avoiding config dependency

--- src/validators/cassette.js
+++ src/validators/cassette.js
@@ -1,18 +1,20 @@
-import {cassetteCombinations, cassetteSpeeds} from '../../config/config';
+import {cassetteCombinations as configCassetteCombinations, cassetteSpeeds as configCassetteSpeeds} from '../../config/config';
 import isArray from 'lodash/isArray';
 import difference from 'lodash/difference';
 import some from 'lodash/some';
 import map from 'lodash/map';
 import flattenDeep from 'lodash/flattenDeep';
-import flatten from 'lodash/flatten';
 
-export default function validate(cassette){
-  const validCassetteSpeeds = flattenDeep(cassetteSpeeds);
+export default function validate(
+  cassette,
+  validCassetteCombinations = configCassetteCombinations,
+  validCassetteSpeeds = configCassetteSpeeds
+){
+  const _validCassetteSpeeds = flattenDeep(validCassetteSpeeds);
+  if(!isArray(cassette) | (!validCassetteSpeed(_validCassetteSpeeds, cassette.length)))
+    throw new Error(`Invalid number of sprockets in the cassette, found ${cassette.length} but must be one of: ${_validCassetteSpeeds}.`);
 
-  if(!isArray(cassette) | (!validCassetteSpeed(validCassetteSpeeds, cassette.length)))
-    throw new Error(`Invalid number of sprockets in the cassette, found ${cassette.length} but must be one of: ${validCassetteSpeeds}.`);
-
-  if(!validCassetteCombination(cassetteCombinations, cassette)){
+  if(!validCassetteCombination(validCassetteCombinations, cassette)){
     throw new Error(`Cassette combination ${cassette} is not supported.`);
   }
 }
@@ -21,9 +23,8 @@ const validCassetteSpeed = (validCassetteSpeeds, cassetteLength) => {
   return some(validCassetteSpeeds, (x) => x === cassetteLength);
 };
 
-const validCassetteCombination = (cassetteCombinations, cassette) => {
-  const validCassettes = flatten(cassetteCombinations);
-  const results = map(validCassettes, (x) => {
+const validCassetteCombination = (validCassetteCombinations, cassette) => {
+  const results = map(validCassetteCombinations, (x) => {
     return difference(cassette, x).length === 0;
   });
   return some(results, (x) => x === true);
--- src/validators/cassette.spec.js
+++ src/validators/cassette.spec.js
@@ -1,36 +1,51 @@
 import validate from './cassette';
 
-describe('src/validators/cassette', function(){
-  let sut = validate;
+const config = {
+  cassetteCombinations: [
+    [12,13,14,15,16,17,19,21,23,25]
+  ],
+  cassetteSpeeds: [
+    10
+  ]
+};
 
-  context('cassette validation', function(){
-    context('with valid cassette', function(){
+describe('src/validators/cassette', function(){
+
+  let Sut = (cassette) => validate(
+    cassette,
+    config.cassetteCombinations,
+    config.cassetteSpeeds
+  );
+
+  context('cassette validation for 10 speed', function(){
+    context('with valid  cassette', function(){
       it('should not throw', function(){
-        const act = () => sut([12,13,14,15,16,17,19,21,23,25]);
+        const validCassette = [12,13,14,15,16,17,19,21,23,25];
+        const act = () => Sut(validCassette);
         act.should.not.throw();
       });
     });
     context('with invalid cassette', function(){
       it('should throw when sprocket combination is not supported.', function(){
         const invalidInput = [1,2,3,4,5,6,7,8,9,0];
-        const act = () => sut(invalidInput);
+        const act = () => Sut(invalidInput);
         act.should.throw(`Cassette combination ${invalidInput} is not supported.`);
       });
       it('should throw when cassette does not match an array of numbers', function(){
         const invalidInput = "[1,2,9,0]";
-        const act = () => sut(invalidInput);
+        const act = () => Sut(invalidInput);
         act.should.throw(/Invalid number of sprockets in the cassette/);
       });
     });
     context('with invalid cassette size', function(){
       it('should throw when the cassette has an unsupported sprocket count', function(){
         const invalidInput = [12,13,14];
-        const act = () => sut(invalidInput);
+        const act = () => Sut(invalidInput);
         act.should.throw(/Invalid number of sprockets in the cassette/);
       });
-      it('should throw when the cassette has more than 11 sprockets', function(){
-        const invalidInput = [12,13,14,15,16,17,18,19,21,22,23,25];
-        const act = () => sut(invalidInput);
+      it('should throw when the cassette has more than 10 sprockets', function(){
+        const invalidInput = [12,13,14,15,16,17,18,19,21,22,23];
+        const act = () => Sut(invalidInput);
         act.should.throw(/Invalid number of sprockets in the cassette/);
       });
     });
