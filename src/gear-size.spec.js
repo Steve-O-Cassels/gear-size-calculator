65e7111b456dcb48ab0dd9ca4576f057f3e1b4cb
Stephen Cassels <steve.o.cassels@gmail.com>
2017-01-30 22:20:29 +0000

    Refactor gear-size test

--- src/gear-size.spec.js
+++ src/gear-size.spec.js
@@ -2,11 +2,12 @@ import gearSize from './gear-size';
 
 describe('src/gear-size/', function(){
   let sut = gearSize;
-  let chainRing = 39, sprocket = 25;
-  let rimAndTyreDiameterInMm = 672;
 
-  describe('gearSize', function(){
-    context('when provided with valid chainring, sprocket, rimAndTyreDiameterInMm', function(){
+  describe('gearSize', function(){    
+    context('when provided with valid input', function(){
+      let chainRing = 39, sprocket = 25;
+      let rimAndTyreDiameterInMm = 672;
+
       it('should calculate the correct gear size', function(){
         sut(chainRing, sprocket, rimAndTyreDiameterInMm)
         .should.equal(1048.3);
