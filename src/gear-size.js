48612b14c7263006971587b911e6d926dbd4b3bb
Stephen Cassels <steve.o.cassels@gmail.com>
2017-01-31 22:36:41 +0000

    gear-size returns object including gearRatio

--- src/gear-size.js
+++ src/gear-size.js
@@ -3,7 +3,10 @@ import round from 'lodash/round';
 export default function gearSize(chainRing, sprocket, rimAndTyreDiameterInMm){
   const gearRatio = chainRingSprocketRatio(chainRing, sprocket);
   const gearSize = gearRatio * rimAndTyreDiameterInMm;
-  return round(gearSize, 1);
+  return {
+    gearSize: round(gearSize, 1),
+    gearRatio: gearRatio
+  };
 }
 
 const chainRingSprocketRatio = (chainring, sprocket) => {
--- src/gear-size.spec.js
+++ src/gear-size.spec.js
@@ -3,15 +3,19 @@ import gearSize from './gear-size';
 describe('src/gear-size/', function(){
   let sut = gearSize;
 
-  describe('gearSize', function(){    
+  describe('gearSize', function(){
     context('when provided with valid input', function(){
       let chainRing = 39, sprocket = 25;
       let rimAndTyreDiameterInMm = 672;
 
       it('should calculate the correct gear size', function(){
-        sut(chainRing, sprocket, rimAndTyreDiameterInMm)
-        .should.equal(1048.3);
+        const result = sut(chainRing, sprocket, rimAndTyreDiameterInMm);
+        result['gearSize'].should.equal(1048.3);
       });
+      it('should calculate the correct gear ratio', function(){
+        const result = sut(chainRing, sprocket, rimAndTyreDiameterInMm);
+        result['gearRatio'].should.equal(1.56);
+      });      
     });
   });
 });
